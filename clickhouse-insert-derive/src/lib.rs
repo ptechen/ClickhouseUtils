e780074a6f1b8babc9c99c107235cc08b8c9fc4a
陶辰 <taochen@taochendeMacBook-Pro-2.local>
2021-08-31 18:17:00 +0800

    update

---
+++ clickhouse-insert-derive/src/lib.rs
@@ -0,0 +1,41 @@
+extern crate proc_macro2;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::spanned::Spanned;
+use proc_macro2::{Ident, Span};
+
+#[proc_macro_derive(Clickhouse)]
+pub fn insert_derive(input: TokenStream) -> TokenStream {
+    let mut insert_tokens = vec![];
+    let parsed_input: DeriveInput = parse_macro_input!(input);
+    let struct_name = parsed_input.ident;
+    match parsed_input.data {
+        Data::Struct(s) => {
+            if let Fields::Named(name_fields) = s.fields {
+                let a = name_fields.named;
+                for field in a {
+                    let field = field.ident.as_ref().unwrap();
+                    let insert_token = quote! {
+                        .add(#field, vec![self.#field.as_ref()])
+                    };
+                    insert_tokens.push(insert_token);
+                }
+            }
+        }
+        other => { panic!("ToVec is not yet implemented for: {:?} ", other) }
+    }
+    let tokens = quote! {
+        impl Clickhouse for #struct_name {
+            fn insert(&self, mut connection: Connection, table_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+                let block = Block::new(table_name)
+                #(#insert_tokens)*
+                connection.insert(&block).await?;
+                connection.close().await?;
+                OK(())
+            }
+        }
+    };
+    proc_macro::TokenStream::from(tokens)
+}
---
+++ client/README.md
@@ -0,0 +1,5 @@
+This ch_driver is forked from [clickhouse_driver](https://github.com/ddulesov/clickhouse_driver). 
+
+The original is inactive, then it is decided that this fork is maintained here.
+
+NOTE: this client driver has problem in the arch and performance. It would be overhauled in some time.
